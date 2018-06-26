4dd89461389908a0c3e9d08e2777ba44ce1b4714
kiilua <kiilua2374@163.com>
2018-06-26 21:47:39 +0800

    update ver1

--- app/src/main/java/com/example/killua/basicandroid/MainActivity.java
+++ app/src/main/java/com/example/killua/basicandroid/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 public class MainActivity extends Activity {
 
     private  Button btnTv;
-
+    // test git ver1
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

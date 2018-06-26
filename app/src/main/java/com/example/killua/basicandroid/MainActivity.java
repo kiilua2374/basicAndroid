1280c963e5f68a0e435d14a304cf6d722c14b7fa
killua <killua2374@163.com>
2018-06-26 17:28:37 +0800

    init

---
+++ app/src/main/java/com/example/killua/basicandroid/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.killua.basicandroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    private  Button btnTv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnTv = findViewById(R.id.btn_tv);
+
+        btnTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TextViewActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
